694dfdea07a0c4980c061dc0da17bd56fb8af83d
ZoltanBozso <zoltan.bozso@larrum.com>
2021-11-19 15:02:51 +0100

    Initial commit

---
+++ src/main/java/com/incomingtask/mycode/MyCodeApplication.java
@@ -0,0 +1,13 @@
+package com.incomingtask.mycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incomingtask/mycode/MyCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incomingtask.mycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
